49a720589106028d1e9ce1bef41422bd48ee8149
Alexander MacL <maclennanalexander@gmail.com>
2025-02-03 14:43:09 +0100

    cleaning du JSON avec sélection des champs à conserver. Suppression des fichiers anciens.

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ headers = {
 # URL de l'endpoint
 url = 'https://www.mk2.com/_next/data/mOVXSxj35n-aMKdxIktJ2/ile-de-france/films/tous-les-films.json?category=tous-les-films&cinemaGroupId=ile-de-france'
 
+def get_director(cast):
+    """Extraire le réalisateur du casting"""
+    for person in cast:
+        if person.get('personType') == 'Director':
+            return {
+                'firstName': person.get('firstName'),
+                'lastName': person.get('lastName')
+            }
+    return None
+
+def filter_movie_data(movie):
+    """Filtrer les données d'un film pour ne garder que les informations nécessaires"""
+    return {
+        'id': movie.get('id'),
+        'title': movie.get('title'),
+        'synopsis': movie.get('synopsis'),
+        'runTime': movie.get('runTime'),
+        'graphicUrl': movie.get('icon'),  # Renommé icon en graphicUrl
+        'openingDate': movie.get('openingDate'),
+        'director': get_director(movie.get('cast', []))
+    }
+
 try:
     # Effectuer la requête HTTP
     response = requests.get(url, headers=headers)
@@ -18,19 +40,20 @@ try:
     # Récupérer les données JSON
     data = response.json()
     
-    # Extraire uniquement les films du pageProps.pageContent.films
+    # Extraire et filtrer les films
     films = data.get('pageProps', {}).get('pageContent', {}).get('films', [])
+    filtered_films = [filter_movie_data(film) for film in films]
     
     # Générer le nom du fichier avec la date et l'heure
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     filename = f'mk2_films_{timestamp}.json'
     
-    # Sauvegarder les données des films dans un fichier JSON
+    # Sauvegarder les données filtrées des films dans un fichier JSON
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(films, f, ensure_ascii=False, indent=2)
+        json.dump(filtered_films, f, ensure_ascii=False, indent=2)
     
     print(f"Les données des films ont été sauvegardées dans '{filename}'")
-    print(f"Nombre de films récupérés : {len(films)}")
+    print(f"Nombre de films récupérés : {len(filtered_films)}")
     
 except requests.exceptions.RequestException as e:
     print(f"Une erreur s'est produite lors de la requête : {e}")
